369e2985acd05543749d16b7939da77b5eccedce
Derechei <work25th91@gmail.com>
2015-08-02 22:30:23 +0300

    finished 12-th task

--- js/11.js
+++ js/11.js
@@ -1,3 +1,17 @@
 /**
  * Created by aleksandr on 8/2/15.
  */
+;(function(){
+    var paragraph = document.getElementsByTagName("p")[0],
+        first =  document.querySelector("[tabindex='1']"),
+        button =  document.querySelector("[type=button]");
+
+    // Using recursion.
+    function factorial(n){
+        if (n === 1) return 1;
+        return n*factorial(n-1);
+    };
+    button.onclick =  function(){
+        paragraph.innerText ="Factorial of " + first.value + " = " + factorial(+first.value);
+    };
+}());
--- js/12.js
+++ js/12.js
@@ -1,3 +1,23 @@
 /**
  * Created by aleksandr on 8/2/15.
  */
+;(function(){
+    var paragraph = document.getElementsByTagName("p")[0],
+        first =  document.querySelector("[tabindex='1']"),
+        button =  document.querySelector("[type=button]"),
+        res = [0,1],
+        i;
+
+    // Using recursion.
+    function fib(n){
+        for( i = 2; i <= n; i += 1){
+            res.push(res[i-1] + res[i-2]);
+        }
+        console.log(res);
+        return res[n];
+    };
+    button.onclick =  function(){
+        paragraph.innerText ="Fibonaccii number " + first.value + " = " + fib(+first.value);
+        res = [0,1];
+    };
+}());
