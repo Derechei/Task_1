45ad11f6f6cd8f84fe5cc6868e61366776c1edee
Derechei <work25th91@gmail.com>
2015-08-02 21:34:32 +0300

    finished 8-th task

--- js/8.js
+++ js/8.js
@@ -1,3 +1,19 @@
 /**
  * Created by aleksandr on 8/2/15.
  */
+;(function(){
+    var paragraph = document.getElementsByTagName("p")[0],
+        first =  document.querySelector("[tabindex='1']"),
+        second =  document.querySelector("[tabindex='2']");
+        button =  document.querySelector("[type=button]");
+    function min(a,b){
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    };
+    button.onclick =  function(){
+        paragraph.innerText = "Minimum value: " + min(+first.value,+second.value);
+    };
+}());
