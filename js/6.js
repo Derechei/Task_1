fc21f1cba23a622e85393b95432e8f45d0ec9abf
Derechei <work25th91@gmail.com>
2015-08-02 18:51:52 +0300

    finished 6-th task

--- js/6.js
+++ js/6.js
@@ -1,3 +1,24 @@
 /**
  * Created by aleksandr on 8/2/15.
  */
+;(function(){
+    var age;
+    // with for
+    /*for(;;){
+        age =  prompt("Type digit bigger than 100");
+        if (+age > 100 || age == null) {
+            break;
+        }
+    }*/
+
+    // with do
+    do {
+        age =  prompt("Type digit bigger than 100");
+    } while (age <= 100 && age !== null);
+
+    // with while
+    /*age =  prompt("Type digit bigger than 100");
+    while(age <= 100 && age !== null) {
+        age =  prompt("Type digit bigger than 100");
+    }*/
+}());
