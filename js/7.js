8043b1c28fa4c976a51bb3a3b1ae85905984e7db
Derechei <work25th91@gmail.com>
2015-08-02 20:47:37 +0300

    finished 7-th task

--- js/7.js
+++ js/7.js
@@ -1,3 +1,24 @@
 /**
  * Created by aleksandr on 8/2/15.
  */
+;(function(){
+    var min= 2,
+        max= 100,
+        i,
+        j,
+        temp = [],
+        result = [],
+        section = document.getElementsByTagName("section")[0];
+    for(i = min; i <= max; i+=1){
+        for(j = min; j <= max; j+=1){
+            if(i % j === 0){
+                temp.push(j);
+            }
+        }
+        if(temp.length === 1){
+            result.push(temp[0]);
+        };
+        temp = [];
+    }
+    section.innerHTML = result.join(", ");
+}());
