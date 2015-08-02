c7452aace2ea899bc03ce4c65fbb6d26f8d935c3
Derechei <work25th91@gmail.com>
2015-08-02 18:03:59 +0300

    finished 5-th task

--- js/5.js
+++ js/5.js
@@ -1,3 +1,18 @@
 /**
  * Created by aleksandr on 8/2/15.
  */
+;(function(){
+    var section = document.getElementsByTagName("section")[0],
+        age = prompt("Type your age");
+    if (!(age>=18 && age<=65)){
+        section.innerText = "INSIDE";
+    } else {
+        section.innerText = "OUTSIDE";
+    }
+    age = prompt("Type your age");
+    if (age<18 || age>65){
+        section.innerText = "INSIDE";
+    } else {
+        section.innerText = "OUTSIDE";
+    }
+}());
