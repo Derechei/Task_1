259cc96f947c3b4365aa48154e10b7f6290f1fa4
Derechei <work25th91@gmail.com>
2015-08-02 21:42:15 +0300

    finished 9-th task

--- js/9.js
+++ js/9.js
@@ -1,3 +1,18 @@
 /**
  * Created by aleksandr on 8/2/15.
  */
+;(function(){
+    var paragraph = document.getElementsByTagName("p")[0],
+        first =  document.querySelector("[tabindex='1']"),
+        second =  document.querySelector("[tabindex='2']");
+    button =  document.querySelector("[type=button]");
+    function pow(x,n){
+        if(n === 1) return x;
+        return x*pow(x, n-1);
+    };
+    button.onclick =  function(){
+        paragraph.innerText =
+            first.value + "^" +second.value +
+            " = "+ pow(+first.value,+second.value);
+    };
+}());
